974434d0fde3c1b2f5bdb2241e351f7cd82b8dfb
afont1 <afont@student.42nice.fr>
2025-01-30 16:22:05 +0100

    fix include error

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: afont <afont@student.42nice.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:59:58 by afont             #+#    #+#             */
-/*   Updated: 2025/01/30 16:05:45 by afont            ###   ########.fr       */
+/*   Updated: 2025/01/30 16:17:05 by afont            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,4 @@
 #include "all.hpp"
 
 void	parser(int ac, char **av);
+std::vector<std::string> split(const std::string& str, char delimiter);
